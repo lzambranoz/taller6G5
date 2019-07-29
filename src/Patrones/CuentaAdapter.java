cbbe92983239dfae85197f7fffeda707dc67a9e2
lzambranoz <lczambra@espol.edu.ec>
2019-07-29 10:52:16 -0500

    implementacion de cuentaAdapter y TransactionException

---
+++ src/Patrones/CuentaAdapter.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Patrones;
+
+import java.util.Currency;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author scmz2607
+ */
+public class CuentaAdapter implements Cuenta{
+    protected Account cuenta;
+    protected Currency moneda;
+    protected Locale locale;
+
+    public CuentaAdapter(int id, double monto) {
+        this.cuenta = new Account(id,monto);
+        this.locale = Locale.US;
+        this.moneda = Currency.getInstance(locale);
+    }
+
+    @Override
+    public double Balance() {
+        return this.cuenta.getAmount();
+    }
+
+    @Override
+    public boolean Retirar(double monto) {
+        String estado = this.cuenta.withdraw(monto);
+        try {
+            if (estado.startsWith("Error")) {
+                throw new TransactionException("Saldo insuficiente");
+            }
+            return true;
+        } catch (TransactionException ex) {
+            Logger.getLogger(CuentaAdapter.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+
+    }
+
+    @Override
+    public boolean Depositar(int monto, int denominacion) {
+        this.cuenta.deposit(monto);
+        return true;
+    }
+
+    @Override
+    public int getId() {
+        return this.cuenta.getId();
+    }
+}

a385e5e8a8a4a50281b3ecfc8efb9b5dd1c08d17
luiacarr <cluis0152@gmail.com>
2019-07-29 11:18:52 -0500

    Cambio en la interfaz Manejador

--- src/Patrones/Manejador.java
+++ src/Patrones/Manejador.java
@@ -1,6 +1,6 @@
 package Patrones;
 
-public interface Manejador {
+public interface Manejador{
     public void crearManejador(int monto, int denominacion);
     public void setNext(Manejador m);
     public boolean retirar(int monto);

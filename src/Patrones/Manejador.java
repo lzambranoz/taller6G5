4c6e43c1081d9f0e87060a764e57ded7e112477d
Ana Carrion Barboto <anacbarboto@atlassian.com>
2019-07-31 18:47:57 -0500

    Agrego metodo getNext()

--- src/Patrones/Manejador.java
+++ src/Patrones/Manejador.java
@@ -2,6 +2,7 @@ package Patrones;
 
 public interface Manejador{
     public void setNext(Manejador m);
+    public Manejador getNext();
     public boolean retirar(double monto);
     public boolean depositar(int cantidad, int denominacion);
     
